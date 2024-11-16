15de03cf9a72909df6f4443e5f374d31f328b358
Josh Moody <joshmoody24@gmail.com>
2024-11-16 23:45:55 +0000

    creating new creature copies ancestor

--- core/admin.py
+++ core/admin.py
@@ -50,15 +50,12 @@ class SpeciesForm(forms.ModelForm):
         for prey in preys_upon:
             if not instance._time_periods_overlap(prey):
                 raise ValidationError(
-                    f"{instance.name} cannot prey upon {prey.name} as their time periods do not overlap."
-                )
+                        f"{instance.name} cannot prey upon {prey.name} as their time periods do not overlap."
+                        )
 
         return cleaned_data
-
-
 @admin.register(Species)
 class SpeciesAdmin(admin.ModelAdmin):
-    form = SpeciesForm
     list_display = (
         "name",
         "genesis_date_mya",
@@ -72,4 +69,75 @@ class SpeciesAdmin(admin.ModelAdmin):
     list_filter = ("inorganic_energy_source", "region", "biome")
     filter_horizontal = ("preys_upon",)
     autocomplete_fields = ("ancestor",)
-    inlines = [OrganInline]
+
+    def get_form(self, request, obj=None, **kwargs):
+        """
+        Choose the form based on whether the object is being created or updated.
+        """
+        if obj is None:  # Object is being created
+            class SpeciesCreationForm(forms.ModelForm):
+                class Meta:
+                    model = Species
+                    fields = ["name", "ancestor"]
+
+            self.form = SpeciesCreationForm
+        else:  # Object is being updated
+            self.form = SpeciesForm
+        return super().get_form(request, obj, **kwargs)
+
+    def get_inlines(self, request, obj):
+        """
+        Show inlines only when editing an existing species.
+        """
+        if obj:  # Only show inlines if the object exists (i.e., not during creation)
+            return [OrganInline]
+        return []
+
+    def save_model(self, request, obj, form, change):
+        """
+        Handle deep copying of settings and organs from the ancestor species when creating a new species.
+        """
+        is_new = obj.pk is None  # Check if this is a new object
+
+        # Ensure that required fields are set for creation
+        if is_new and obj.ancestor:
+            ancestor = obj.ancestor
+
+            # Copy essential fields from the ancestor
+            obj.description = ancestor.description
+            obj.genesis_date_mya = ancestor.genesis_date_mya
+            obj.extinction_date_mya = ancestor.extinction_date_mya
+            obj.lifetime_years = ancestor.lifetime_years  # Ensure this field is set
+            obj.inorganic_energy_source = ancestor.inorganic_energy_source
+            obj.region = ancestor.region
+            obj.biome = ancestor.biome
+
+        # Save the species instance first
+        super().save_model(request, obj, form, change)
+
+        if is_new and obj.ancestor:
+            # Copy the `preys_upon` Many-to-Many field
+            obj.preys_upon.set(ancestor.preys_upon.all())
+
+            # Deep copy organs
+            ancestor_organs = ancestor.organs.prefetch_related("systems").all()
+
+            new_organs = []
+            system_mappings = {}
+            for organ in ancestor_organs:
+                new_organ = Organ(
+                    name=organ.name,
+                    description=organ.description,
+                    species=obj,
+                )
+                new_organs.append(new_organ)
+
+            # Bulk create new organs
+            Organ.objects.bulk_create(new_organs)
+
+            # Set organ systems for the new organs
+            for new_organ, original_organ in zip(new_organs, ancestor_organs):
+                system_mappings[new_organ] = list(original_organ.systems.all())
+
+            for organ, systems in system_mappings.items():
+                organ.systems.set(systems)
